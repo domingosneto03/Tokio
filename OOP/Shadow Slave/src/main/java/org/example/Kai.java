7956b9ec8ecc876c0d87a0f0030b7811aed0bf1b
domingosneto03 <domingos12126@gmail.com>
2024-02-05 04:10:08 +0000

    Shadow Slave Project

---
+++ OOP/Shadow Slave/src/main/java/org/example/Kai.java
@@ -0,0 +1,49 @@
+package org.example;
+
+public class Kai implements Lutador {
+
+    private String name;
+    private int hp;
+    private int attackPoints;
+
+    public Kai(String name, int hp, int attackPoints) {
+        this.name = name;
+        this.hp = hp;
+        this.attackPoints = attackPoints;
+    }
+
+    @Override
+    public int atacar(int hpAdversario) {
+        hpAdversario -= attackPoints;
+        if (hpAdversario <= 0) hpAdversario = 0;
+        return hpAdversario;
+    }
+
+    @Override
+    public void defender() {
+        hp += 5;
+        if (hp > 100) hp = 100;
+    }
+
+    @Override
+    public void obterVida() {
+        hp += 20;
+        if (hp > 100) hp = 100; // máximo de vida
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getAttackPoints() {
+        return attackPoints;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
