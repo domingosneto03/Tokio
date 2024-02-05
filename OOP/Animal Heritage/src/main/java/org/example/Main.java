a2831d89e74dfab81acc717767d5392795e645e5
domingosneto03 <domingos12126@gmail.com>
2024-02-05 03:57:23 +0000

    Animal Heritage Project

---
+++ OOP/Animal Heritage/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        System.out.println("Vamos brincar com os animais!");
+        menu.mainMenu();
+    }
+}
