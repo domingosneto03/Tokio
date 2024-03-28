027c1a733e38546d762ff18db1b2d9106351430e
domingosneto03 <domingos12126@gmail.com>
2024-03-28 16:58:16 +0000

    bag

---
+++ OCP/Module 1/Classes and Methods/Bag/src/main/java/org/example/Bag.java
@@ -0,0 +1,33 @@
+package org.example;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Bag {
+    private List<String> items;
+    private Random random;
+
+    public Bag() {
+        items = new ArrayList<>();
+        random = new Random();
+    }
+
+    public List<String> getItems() {
+        return items;
+    }
+
+    // Método para adicionar itens à Bag
+    public void putInBag(List<String> itemsToAdd) {
+        items.addAll(itemsToAdd);
+    }
+
+    // Método para remover e retornar um elemento aleatório da Bag
+    public Object takeFromBag() {
+        if (items.isEmpty()) {
+            throw new IllegalStateException("A sacola está vazia.");
+        }
+        int randomIndex = random.nextInt(items.size());
+        return items.remove(randomIndex);
+    }
+}
+
