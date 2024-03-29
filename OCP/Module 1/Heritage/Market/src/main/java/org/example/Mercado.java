71458be6896a11730478fd0a99bbbc9290ac2884
domingosneto03 <domingos12126@gmail.com>
2024-03-29 17:27:11 +0000

    mercado

---
+++ OCP/Module 1/Heritage/Market/src/main/java/org/example/Mercado.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Mercado {
+    public static void funcionariosMercado(Trabalhadores[] listaFuncionarios) {
+        for(Trabalhadores x : listaFuncionarios) {
+            System.out.println(
+                    "Nome: " + x.getNome() + "\n" +
+                    "Apelido: " + x.getApelido() + "\n" +
+                    "Número de Identificação: " + x.getId() + "\n" +
+                    "Morada: " + x.getMorada() + "\n" +
+                    "Telefone: " + x.getTelefone() + "\n" +
+                    "Número da Segurança Social: " + x.getNrSegSocial() + "\n" +
+                    "Turno: " + x.getTurno() + "\n" +
+                    "Salário: " + x.salarioTurno() + ".00€"
+            );
+            System.out.println("-------------------");
+        }
+    }
+}
