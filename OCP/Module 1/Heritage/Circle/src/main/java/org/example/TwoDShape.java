dbeabef5d877ccd1ae583c11e2f3a65818bf0999
domingosneto03 <domingos12126@gmail.com>
2024-03-29 14:01:43 +0000

    circle

---
+++ OCP/Module 1/Heritage/Circle/src/main/java/org/example/TwoDShape.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class TwoDShape {
+    protected double base;
+    protected double altura;
+
+    public TwoDShape(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+}
