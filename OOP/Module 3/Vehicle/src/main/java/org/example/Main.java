4f6abf4888b13fa0adab0e7a605c8787a8ad99c4
domingosneto03 <domingos12126@gmail.com>
2024-04-16 17:41:27 +0100

    init refactor15

--- OOP/Module 3/Vehicle/src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Vamos ver a tua garagem");
-        Menu menu = new Menu();
-        menu.mainMenu();
-    }
-}
