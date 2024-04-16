d034f13fa6a4289d87b12a80bb3dc189e32d54de
domingosneto03 <domingos12126@gmail.com>
2024-04-16 17:20:28 +0100

    refactor3 complete

---
+++ OOP/Module 2/Guess The Number/src/main/java/org/example/Game.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Game {
+    public boolean didGuess(int answer, int userOption) {
+        if(userOption == answer) {
+            System.out.println("Congratulations! You got it right!!!");
+            return true;
+        }
+        else if(userOption < answer) {
+            System.out.println("Hmm the correct number is higher.");
+            return false;
+        }
+        else {
+            System.out.println("Hmm the correct number is lower.");
+            return false;
+        }
+    }
+}
